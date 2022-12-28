15e6cbaba9336a2c5f2f39d476103cc060ec80de
Alfat <alfat.cse.21@gmail.com>
2022-12-28 14:55:32 +0100

    mongodb auth problem fixed

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,35 @@
-module.exports.register = (req, res, next) => {
-  const {username,email,password}=req.body;
-  const usernameCheck=await
+const User = require("../model/userModel");
+const brcypt = require("bcrypt");
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://alfatjahan:TkeLn78oZoIYU5Gk@cluster0.icjdeya.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+module.exports.register = async (req, res, next) => {
+   
+  try {
+    const User = client.db("ChatUser").collection("Users");
+    const { username, email, password } = req.body;
+    console.log(req.body);
+    const usernameCheck = await User.insertOne({ username });
+    // if (usernameCheck) {
+    //   return res.json({ msg: "Username already used", status: false });
+    // }
+    // const emailCheck = await User.findOne({ email });
+    // if (emailCheck) {
+    //   return res.json({ msg: "Email already used", status: false });
+    // }
+    // const hashedPassword = await brcypt.hash(password, 10);
+    // const user = await User.insertOne({
+    //   email,
+    //   username,
+    //   password: hashedPassword,
+    // });
+    // delete user.password;
+    // return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
 };
--- index.js
+++ index.js
@@ -1,21 +1,58 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const userRoutes=require('./routes/userRoutes')
-const app=express();
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const brcypt = require("bcrypt");
+const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
+const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.set('strictQuery', true);
-app.use('/api/auth',userRoutes);
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    
-}).then(()=>{
-    console.log('DB connection done')
-}).catch((e)=>console.log(e))
-const server=app.listen(process.env.PORT,()=>{
-    console.log(`Server is running ${process.env.PORT}`)
-})
-app.get('/',async(req,res)=>res.send('Chatting server is running'));
+const port = 5009;
+const http = require("http");
+const { Server } = require("socket.io");
+const { isObject } = require("util");
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.icjdeya.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+async function run() {
+  const ChatUser = client.db("Chat").collection("Chatuser");
+  try {
+    app.post("/register", async (req, res) => {
+      const { email, username, password } = req.body;
+      const usernameCheck = await ChatUser.findOne({ username });
+      if (usernameCheck) {
+        return res.json({ msg: "Username already used", status: false });
+      }
+      const emailCheck = await ChatUser.findOne({ email });
+      if (emailCheck) {
+        return res.json({ msg: "Email already used", status: false });
+      }
+      const hashedPassword = await brcypt.hash(password, 10);
+      const user = await ChatUser.insertOne({
+        email,
+        username,
+        password: hashedPassword,
+      });
+      delete user.password;
+      return res.json({ status: true, user });
+    });
+  } finally {
+  }
+}
+run().catch((e) => console.log(e));
+app.listen(port, () => {
+  console.log(`Serveddr is runningf ${port}`);
+});
